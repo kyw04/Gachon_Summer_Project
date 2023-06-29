86730260f0834f9a22def8b48cbb6941c238b7fc
Junyuhan04 <junyuhan04@gmail.com>
2023-06-29 18:23:10 +0900

    Title Scene, Loading Scene
    타이틀 씬 제작 및 로딩창 구현

---
+++ Assets/Scripts/BtnManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BtnManager : MonoBehaviour
+{
+    private int sceneNum; // 각 Scene들의 번호를 알려주기 위한 변수
+
+
+    public void OnClick_StartBtn() // Title에서 Start 눌렀을때 실행되는 함수
+    {
+        sceneNum = 1;
+        StartCoroutine(Scene_Move());
+    }
+
+    public void OnClick_QuitBtn()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif 
+        Application.Quit();
+    }
+
+    IEnumerator Scene_Move()
+    {
+        yield return new WaitForSeconds(0.5f);
+        switch (sceneNum)
+        {
+            case 1:
+                LoadingScene.LoadScene("JYH");
+                break;
+        }
+    }
+}
