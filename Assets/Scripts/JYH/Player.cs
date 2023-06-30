d05b11435f8a561cba6f95ab06cad45cfbc2e80d
lawsuit3310 <aimeet5480@gmail.com>
2023-06-30 21:19:56 +0900

    20230630
    merge 했을 때 컴파일 에러 뜨는 거 수정

--- Assets/Scripts/JYH/Player.cs
+++ Assets/Scripts/JYH/Player.cs
@@ -2,46 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+namespace JYH
 {
-    CharacterController cc;
-    public float gravityScale;
-    public float jumpPower;
-    float y;
-    public float Speed;
-    public float CurSpeed;
-
-    void Start()
+    public class Player : MonoBehaviour
     {
-        cc = GetComponent<CharacterController>();
-        CurSpeed = Speed = 5;
-    }
+        CharacterController cc;
+        public float gravityScale;
+        public float jumpPower;
+        float y;
+        public float Speed;
+        public float CurSpeed;
 
-    // Update is called once per frame
-    void Update()
-    {
-        Move();
-    }
-
-    void Move()
-    {
-        y -= gravityScale * Time.deltaTime;
-
-        if (Input.GetKeyDown(KeyCode.Space) && cc.isGrounded)
+        void Start()
         {
-            y = jumpPower;
+            cc = GetComponent<CharacterController>();
+            CurSpeed = Speed = 5;
         }
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
 
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-            CurSpeed = Speed + 3;
-        else if (Input.GetKeyUp(KeyCode.LeftShift))
-            CurSpeed = Speed;
+        // Update is called once per frame
+        void Update()
+        {
+            Move();
+        }
 
-        Vector3 dir = new Vector3(h, y, v);
-        dir = transform.TransformDirection(dir);
-        dir.y = y;
-        cc.Move(dir * CurSpeed * Time.deltaTime);
+        void Move()
+        {
+            y -= gravityScale * Time.deltaTime;
+
+            if (Input.GetKeyDown(KeyCode.Space) && cc.isGrounded)
+            {
+                y = jumpPower;
+            }
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+                CurSpeed = Speed + 3;
+            else if (Input.GetKeyUp(KeyCode.LeftShift))
+                CurSpeed = Speed;
+
+            Vector3 dir = new Vector3(h, y, v);
+            dir = transform.TransformDirection(dir);
+            dir.y = y;
+            cc.Move(dir * CurSpeed * Time.deltaTime);
+        }
     }
+
 }
