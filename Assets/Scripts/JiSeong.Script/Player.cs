5d7dddae11d70b2c816f751e0402572f2b4cce60
yoonjiseang <imzzang3u@naver.com>
2023-06-30 18:56:59 +0900

    잡몹 테스또

---
+++ Assets/Scripts/JiSeong.Script/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    protected Rigidbody rb;
+    protected float moveSpeed = 0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(Vector3.forward * moveSpeed);
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(Vector3.back * moveSpeed);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Translate(Vector3.left * moveSpeed);
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Translate(Vector3.right * moveSpeed);
+        if (Input.GetKey(KeyCode.U))
+            transform.Translate(Vector3.up * moveSpeed);
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.down * moveSpeed);
+    }
+}
