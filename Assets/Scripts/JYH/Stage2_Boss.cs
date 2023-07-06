5742191aa0c2a074c01ef40b4e3bdfa2a875d6c5
Junyuhan04 <junyuhan04@gmail.com>
2023-07-06 18:46:32 +0900

    2stage boss pattern

--- Assets/Scripts/JYH/Stage2_Boss.cs
+++ Assets/Scripts/JYH/Stage2_Boss.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using JYH;
 using System.Collections;
 using System.Collections.Generic;
@@ -10,33 +11,29 @@ public class Stage2_Boss : MonoBehaviour
     Animator anim;
     NavMeshAgent agent;
 
+
     Rigidbody rb;
-    [SerializeField] int Boss_AttackNum;
 
-    [SerializeField] int ranNum;
+    [Header("원형으로 공격하는 프리팹 관련 변수들")]
+    public float meteorSpeed;
+    [SerializeField] Transform[] spawnPoint;
 
-    public ObjectPoolComponent boss_Attack;
-    float attackDelayTime;
 
+    public ObjectPoolComponent[] boss_Attack;
     public GameObject die_Effect;
 
     float away;
     public Transform Player;
-
-    // Transform playerTransform;
-    IEnumerator attack;
-
     Vector3 dir;
 
-    public float dirX;
-    public float dirZ;
+    float delay_Time;
 
+    public float dirZ;
     public enum EnemyState
     {
         Idle,
         Walk,
-        Attack1,
-        Attack2,
+        Attack,
         Damaged,
         Dead
     }
@@ -51,26 +48,16 @@ public class Stage2_Boss : MonoBehaviour
     }
     void Start()
     {
+
         Player = FindObjectOfType<PlayerCtrl>().transform;
-
         eState = EnemyState.Idle;
-
-        //GameObject player = GameObject.Find("Player");
-        //if (player != null)
-        //{
-        //    playerTransform = player.transform;
-        //}
-        attack = Attack_C();
-        StartCoroutine(attack);
-        //  StartCoroutine(Move_C());
-
-
-
     }
 
     void Update()
     {
-        away = Vector3.Distance(transform.position, Player.position);
+        away = Vector3.Distance(transform.position, Player.position); // 적과 플레이어의 거리
+        Vector3 playerPosition = Player.position; // 플레이어의 위치
+
         //  Debug.Log(away);
 
         if (Input.GetKeyDown(KeyCode.Q))
@@ -88,10 +75,10 @@ public class Stage2_Boss : MonoBehaviour
                 break;
 
             case EnemyState.Walk:
-                Walk(dir);
+                Walk();
                 break;
 
-            case EnemyState.Attack1:
+            case EnemyState.Attack:
                 Attack();
                 break;
 
@@ -102,124 +89,117 @@ public class Stage2_Boss : MonoBehaviour
     }
     public void Idle()
     {
-        // 플레이어와 약간 가까워지면
-        if (away <= 10)
-        {
-            // 이동 상태로 전환
-            eState = EnemyState.Walk;
-            // 이동 애니메이션으로 전환
-            anim.SetFloat("Z", 1);
-        }
+        agent.isStopped = true; // 길찾기 종료
+        StartCoroutine(Idle_Delay());
+        eState = EnemyState.Walk;
     }
-
-    public void Walk(Vector3 dir)
+    IEnumerator Idle_Delay()
     {
-        //dir = Vector3.Lerp(dir, new Vector3(dirX, 0, dirZ), Time.deltaTime * 5);
-        //anim.SetFloat("Z", dir.z);
-        //anim.SetFloat("X", dir.x);
-
-        if (away <= 5)
-        {
-            // 공격 상태가 되자마자 때리도록
-            attackDelayTime = 1;
-            eState = EnemyState.Attack1;
-        }
-        // 거리가 멀어졌을 때 기본 상태
-        else if (away > 10)
-        {
-            eState = EnemyState.Idle;
-
-            // 기본 애니메이션으로 전환
-            anim.SetFloat("Z", 0);
-
-            // 길찾기 중지
-            agent.isStopped = true;
-        }
-        //플레이어를 향해 이동
-        else
-        {
-            Debug.Log("플레이어에게 가는중");
-            anim.SetFloat("Z", 1);
-            // 계속해서 플레이어를 향해 이동하도록 목적지 설정 (destination)
-            agent.destination = Player.position;
-            // 길찾기 시작
-            agent.isStopped = false;
-        }
+        anim.StopPlayback();
+        yield return new WaitForSeconds(2f);
     }
-
+    public void Walk()
+    {
+        agent.destination = Player.position;     // 계속해서 플레이어를 향해 이동하도록 목적지 설정 (destination)
+        agent.isStopped = false; // 길찾기 시작
+        // dir = Vector3.Lerp(dir, new Vector3(0, 0, dirZ), Time.deltaTime * 5);
+        // anim.SetFloat("Z", dir.z);
+        anim.SetFloat("Z", 1);
+        if (away <= 10)
+            eState = EnemyState.Attack;
+    }
     void Dead()
     {
         die_Effect.SetActive(true);
-        anim.SetTrigger("dead"); 
-        Destroy(gameObject, 5f);
-        Destroy(die_Effect, 5f);
+        anim.SetTrigger("dead");
     }
     void Attack()
     {
-        // 진행 시간 누적
-        attackDelayTime += Time.deltaTime;
-
-        // 1초가 지나면
-        if (attackDelayTime >= 1)
+        if (delay_Time <= Time.time)
         {
-            // 공격 애니메이션으로 전환
-            anim.SetTrigger("attack");
+            delay_Time = Time.time + 3f;
 
-            // 0으로 초기화해서 다시 처음부터 1초 세기
-            attackDelayTime = 0;
-        }
-        // 플레이어가 멀어지면
-        if (away > 5)
-        {
-            // 이동 상태로 전환
-            eState = EnemyState.Walk;
-        }
-    }
-
-    IEnumerator Move_C()
-    {
-        while (true)
-        {
-            dirX = Random.Range(-1, 2);
-            dirZ = Random.Range(-1, 2);
-
-            rb.velocity = new Vector3(dirX, 0, dirZ);
-            yield return new WaitForSeconds(2f);
-
-        }
-    }
-
-    IEnumerator Attack_C()
-    {
-        while (true)
-        {
-            switch (Boss_AttackNum)
+            int attackNum = Random.Range(1, 4);
+            Debug.Log(attackNum);
+            switch (attackNum)
             {
-                // 낫 한번 휘두르기 
                 case 1:
-
+                    Slash();
                     break;
-                // 낫 두번 휘두르기 
                 case 2:
-
+                    CircleMeteorAttack();
                     break;
-                // 마법 공격(메테오 소환)
                 case 3:
-                    // 플레이어의 위치를 얻어옴
-                    Vector3 playerPosition = Player.position;
-                    // 메테오를 플레이어의 현재 위치에 생성
-                    Vector3 meteorPosition = new Vector3(playerPosition.x, playerPosition.y + 3f, playerPosition.z);
-                    GameObject meteor = boss_Attack.GetItem(meteorPosition);
-                    meteor.GetComponent<Meteor_2Stage>().boss_Attack = this.boss_Attack;
-                    //// GameObject meteor = Instantiate(meteorPrefab, meteorPosition, Quaternion.identity);
-                    if (meteor.transform.position.y <= -0.5)
-                        boss_Attack.FreeItem(meteor);
-
-
-                    // 생성된 메테오에 대한 추가 설정이 필요하다면 여기에 추가 코드 작성
+                    SpawnMeteor_Attack();
                     break;
             }
-            yield return new WaitForSeconds(2f);
         }
+    }
+
+    public void Slash()
+    {
+        if (away <= 5)
+            anim.SetTrigger("Slash");
+    }
+    public void CircleMeteorAttack() // 원형으로 메테오 생성
+    {
+        anim.SetTrigger("shootMeteor");
+        Debug.Log("원형 메테오");
+    }
+
+    void Real_CircleMeteor_Attack()
+    {
+        for (int i = 0; i < spawnPoint.Length; i++)
+        {
+            Debug.Log("Real_CircleMeteor_Attack 들어옴");
+            GameObject meteor0 = boss_Attack[1].GetItem(spawnPoint[i].position);
+            Vector3 direction = spawnPoint[i].position - transform.position; // 현재 위치에서 spawnPoint 위치로 향하는 벡터
+            meteor0.GetComponent<Rigidbody>().velocity = direction.normalized * meteorSpeed;
+        }
+    }
+
+    void Off_Speed()
+    {
+        agent.speed = 0;
+        //  eState = EnemyState.Idle;
+    }
+
+    void On_Speed()
+    {
+        agent.speed = 3.5f;
+    }
+
+
+    void SpawnMeteor_Attack() // 위에서 메테오 생성
+    {
+        anim.SetTrigger("spawn_Meteor");
+        Debug.Log("sky메테오");
+    }
+
+    void Real_SpawnMeteor_Attack()
+    {
+        //    Debug.Log("Real_SpawnMeteor_Attack 들어옴");
+        // 메테오를 플레이어의 현재 위치에 생성
+        Vector3 meteorPosition = new Vector3(Player.position.x, Player.position.y + 3f, Player.position.z);
+        GameObject meteor = boss_Attack[0].GetItem(meteorPosition);
+        meteor.GetComponent<Meteor_2Stage>().boss_Attack = boss_Attack[0];
+        if (meteor.transform.position.y <= -0.5)
+            boss_Attack[0].FreeItem(meteor);
+
+        eState = EnemyState.Idle;
+
+    }
+
+    void RealDead()
+    {
+        Destroy(gameObject);
+    }
+    void RealAttack()
+    {
+        if (away <= 3)
+        {
+            Player.SendMessage("Damaged", 0.2f);
+        }
+        eState = EnemyState.Idle;
     }
 }
