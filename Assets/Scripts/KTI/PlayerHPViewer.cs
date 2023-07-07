94b83940c79898d6c891e694b857cd52247bf2e8
Junyuhan04 <junyuhan04@gmail.com>
2023-07-07 15:50:33 +0900

    7/7 - 15:49

--- Assets/Scripts/KTI/PlayerHPViewer.cs
+++ Assets/Scripts/KTI/PlayerHPViewer.cs
@@ -14,13 +14,13 @@ public class PlayerHPViewer : MonoBehaviour
     private float P_maxHp = 100f; // 플레이어 최대 체력
     private float P_curHp = 100f; // 플레이어 현재 체력
 
-    
+
 
 
     void Start()
     {
-       P_hpbar.value = (float)P_curHp / (float)P_maxHp;
-       P_hpbar.minValue = 0;
+        P_hpbar.value = (float)P_curHp / (float)P_maxHp;
+        P_hpbar.minValue = 0;
     }
 
 
@@ -31,23 +31,35 @@ public class PlayerHPViewer : MonoBehaviour
     {
         P_hpbar.maxValue = P_maxHp;
         P_hpbar.value = P_curHp;
-        hp_text.text = (P_curHp.ToString() + "/" + P_maxHp.ToString());
 
         if (Input.GetKeyDown(KeyCode.V))
         {
             if (P_curHp > 0)
             {
                 P_curHp -= 13.0f;
+                hp_text.text = (P_curHp.ToString() + "/" + P_maxHp.ToString());
             }
-
         }
         if (P_hpbar.value <= 0)
-        {     
-                SceneManager.LoadScene(4);  
+        {
+            P_hpbar.value = 0;
+            hp_text.text = (P_curHp.ToString() + "/" + P_maxHp.ToString());
+            SceneManager.LoadScene(4);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Meteor":
+                //        Player.SendMessage("Damaged", 0.2f);
+                break;
+            case "CircleMeteor":
+                //        Player.SendMessage("Damaged", 0.2f);
+                break;
         }
     }
     // 임시로 V키를 눌렀을 때 피가 깎이도혹 함 
     // 적의 공격에 맞았을 때로 코드 수정해야 함
-
 }
 
