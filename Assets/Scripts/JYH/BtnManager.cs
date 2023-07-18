cde265abb045c31cf513a253b0d77d57c53de068
KTI <runaway040722@naver.com>
2023-07-18 14:03:02 +0900

    kti

--- Assets/Scripts/JYH/BtnManager.cs
+++ Assets/Scripts/JYH/BtnManager.cs
@@ -24,7 +24,12 @@ public class BtnManager : MonoBehaviour
     }
     void Update()
     {
+<<<<<<< Updated upstream
         if (Input.GetKeyDown(KeyCode.Escape) && sceneNum == 3)
+=======
+        Debug.Log(sceneNum);
+        if (Input.GetKeyDown(KeyCode.Escape))
+>>>>>>> Stashed changes
         {
             Time.timeScale = 0;
             Cursor.visible = true;
